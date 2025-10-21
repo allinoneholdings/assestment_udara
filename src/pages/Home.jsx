9d3f643f160e57e4988685461e6897500b6b25d5
udarajayasundara <udaradananjayabandara1000@gmail.com>
2025-10-21 15:17:07 +0530

    Brief and meaningful description of my changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import Header from '../components/Header.jsx';
-import Body from '../components/Body.jsx';
-import Footer from '../components/Footer.jsx';
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <main>
-        <Body />
-        </main>
-        <Footer />
     </div>
   );
 }

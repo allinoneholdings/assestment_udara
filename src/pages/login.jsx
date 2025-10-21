04a2718a4c9a86f9eac152be894e3ad53396755a
udarajayasundara <udaradananjayabandara1000@gmail.com>
2025-10-21 17:18:32 +0530

    did some small changes to the codebase

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function Login() {
 
-    const [email, setEmail] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
